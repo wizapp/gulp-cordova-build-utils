ff9f78778ef8596a21121c57c9ce3d2425594541
Sebastien Royer <sroyer@wizapp.fr>
2017-12-01 07:01:00 +0100

    Add GULP.CORDOVA_APP_NAME replacement variable

--- inject-cordova-config.js
+++ inject-cordova-config.js
@@ -18,7 +18,7 @@ const DEFAULT_SOURCE = 'index.html';
  * @param {string[]} options.versionNumber Version number of the release
  * @param {string} options.source Path of the application, e.g. index.html (or http://localhost:post for iOS)
  */
-module.exports = function injectConfig({ appId, versionNumber, source = DEFAULT_SOURCE} = options) {
+module.exports = function injectConfig({ appId, appName, versionNumber, source = DEFAULT_SOURCE} = options) {
 	// Split version into major/minor/patch, and calculate a version code for android versioning
 	const versionTokens = versionNumber.split('.');
 	const androidVersionCode = versionTokens[0]*100000 + versionTokens[1]*1000 + versionTokens[2]*10
@@ -28,6 +28,7 @@ module.exports = function injectConfig({ appId, versionNumber, source = DEFAULT_
 	return lazypipe()
 		.pipe(() => configFilter)
 		.pipe(() => replace('%%GULP.CORDOVA_ID%%', appId))
+		.pipe(() => replace('%%GULP.CORDOVA_APP_NAME%%', appName))
 		.pipe(() => replace('%%GULP.CORDOVA_VERSION%%', versionNumber))
 		.pipe(() => replace('%%GULP.CORDOVA_VERSION_ANDROID_VERSION_CODE%%', androidVersionCode))
 		.pipe(() => replace('%%GULP.CORDOVA_SOURCE%%', source))
